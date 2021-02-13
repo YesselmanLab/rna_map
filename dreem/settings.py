7a636a2bd3025c1b2b1123496205e0cdbd8b06e7
Joe Yesselman <jyesselm@unl.edu>
2021-02-13 16:42:55 -0600

    more minor fixes

--- dreem/settings.py
+++ dreem/settings.py
@@ -4,5 +4,5 @@ import os
 def get_lib_path():
     file_path = os.path.realpath(__file__)
     spl = file_path.split("/")
-    base_dir = "/".join(spl[:-1])
+    base_dir = "/".join(spl[:-2])
     return base_dir
