327df77544fa9bc3cb73a68e8fdbf45847318fdc
Joe Yesselman <jyesselm@unl.edu>
2021-12-23 19:01:23 -0600

    fixed pytest issues

--- test/test_parameters.py
+++ test/test_parameters.py
@@ -4,7 +4,8 @@ import os
 from click.testing import CliRunner
 from dreem import parameters, settings, run, run_docker
 import dreem
-TEST_DIR = os.path.dirname( os.path.realpath( __file__ ) )
+
+TEST_DIR = os.path.dirname(os.path.realpath(__file__))
 BASE_DIR = os.path.dirname(TEST_DIR)
 
 
@@ -12,24 +13,26 @@ def get_default_args():
     # TODO joe I fixed these params so it would run but idk if these
     # are correct -CJ
     p = {
-         'fasta'     : TEST_DIR + '/resources/case_1/test.fasta',
-         'fastq1'    : TEST_DIR + '/resources/case_1/test_mate1.fastq',
-         'fastq2'    : TEST_DIR + '/resources/case_1/test_mate2.fastq',
-         'overwrite' : False,
-         'csv'       : None,
-         'param_file': None,
-         'log_level' : 'INFO',
-         'bv_overwrite': False,
-         'restore_org_behavior': False,
-         'bt2_alignment_args': None,
-         'qscore_cutoff': None,
-         'num_of_surbases': None,
-         'map_score_cutoff': None,
-         'percent_length_cutoff': None,
-         'mutation_count_cutoff': None,
-         'skip_fastqc': False,
-         'skip_trim_galore': False
-         }
+        'fasta'                : TEST_DIR + '/resources/case_1/test.fasta',
+        'fastq1'               : TEST_DIR + '/resources/case_1/test_mate1.fastq',
+        'fastq2'               : TEST_DIR + '/resources/case_1/test_mate2.fastq',
+        'overwrite'            : False,
+        'csv'                  : None,
+        'param_file'           : None,
+        'log_level'            : 'INFO',
+        'bv_overwrite'         : False,
+        'restore_org_behavior' : False,
+        'bt2_alignment_args'   : None,
+        'qscore_cutoff'        : None,
+        'num_of_surbases'      : None,
+        'map_score_cutoff'     : None,
+        'percent_length_cutoff': None,
+        'mutation_count_cutoff': None,
+        'skip_fastqc'          : False,
+        'skip_trim_galore'     : False,
+        'plot_sequence'        : False,
+        'summary_output_only'  : False
+    }
     return p
 
 
@@ -54,6 +57,7 @@ def test_parse_new_param_file():
     assert params.map.bt2_alignment_args == \
            "--local,--no-unal,--no-discordant,--no-mixed,-X 1000,-L 20,-p 16"
 
+
 def test_write_to_yaml():
     p = get_default_args()
     params = parameters.ParametersFactory().get_parameters(p)
@@ -69,9 +73,10 @@ def test_get_sub_params():
     map = pf._Map()
     assert map.skip == False
 
+
 @pytest.mark.integration
 def test_help_strings():
-    open('loc','w').write(dreem.__file__)
+    open('loc', 'w').write(dreem.__file__)
     runner = CliRunner()
 
     args = [
